b58a7e74917866f0faf327265893f735c1fc8541
cambrwn846 <cambrwn846@localhost>
2024-10-17 20:55:29 -0400

    commit 2 q3 votingmachine

--- LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question3/VotingMachine.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/midterm/question3/VotingMachine.java
@@ -4,9 +4,6 @@ package org.howard.edu.lsp.midterm.question3;
 import java.util.HashMap;
 import java.util.Map;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class VotingMachine {
     private Map<String, Integer> candidates; 
 
