7286a4798bc3d96a680b9f8f5b82c1837d5c842d
cambrown846 <152243337+cambrown846@users.noreply.github.com>
2024-12-05 20:38:24 -0500

    Update ShapeFactoryTest.java
    final q3 commit 3

--- LSP_Fall_2024/src/org/howard/edu/lsp/finalexam/question3/ShapeFactoryTest.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/finalexam/question3/ShapeFactoryTest.java
@@ -1,14 +1,54 @@
+// Camryn Brown
+
 package org.howard.edu.lsp.finalexam.question3;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-
+//JUnit 5
 class ShapeFactoryTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    private ShapeFactory shapeFactory;
 
+    @BeforeEach
+    void setUp() {
+        shapeFactory = ShapeFactory.getInstance();  
+    }
+
+ 
+    @Test
+    void testGetCircleShape() {
+        Shape shape = shapeFactory.getShape("circle");
+        assertNotNull(shape, "Shape should not be null");
+        assertTrue(shape instanceof Circle, "Shape should be an instance of Circle");
+    }
+
+    @Test
+    void testGetRectangleShape() {
+        Shape shape = shapeFactory.getShape("rectangle");
+        assertNotNull(shape, "Shape should not be null");
+        assertTrue(shape instanceof Rectangle, "Shape should be an instance of Rectangle");
+    }
+
+
+    @Test
+    void testGetTriangleShape() {
+        Shape shape = shapeFactory.getShape("triangle");
+        assertNotNull(shape, "Shape should not be null");
+        assertTrue(shape instanceof Triangle, "Shape should be an instance of Triangle");
+    }
+
+    @Test
+    void testGetUnknownShape() {
+        Shape shape = shapeFactory.getShape("hexagon");
+        assertNull(shape, "Shape should be null for unknown type");
+    }
+
+  
+    @Test
+    void testSingletonShapeFactory() {
+        ShapeFactory anotherShapeFactory = ShapeFactory.getInstance();
+        assertSame(shapeFactory, anotherShapeFactory, "Both instances should be the same (singleton)");
+    }
 }
